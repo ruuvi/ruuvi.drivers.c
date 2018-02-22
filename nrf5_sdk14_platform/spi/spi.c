db1267530c9cc7c6bace5afb397f03ca70278af4
Otso Jousimaa <otso@ojousima.net>
2018-02-22 14:41:09 +0800

    SPI Bosch platform WIP

--- nrf5_sdk14_platform/spi/spi.c
+++ nrf5_sdk14_platform/spi/spi.c
@@ -40,6 +40,7 @@
 #include "sdk_application_config.h"
 #ifdef NRF_SDK14_SPI
 #include <stdint.h>
+#include <string.h> //memcpy
 
 #include "spi.h"
 #include "boards.h"
@@ -50,13 +51,22 @@
 #include "ruuvi_error.h"
 #include "yield.h"
 
-#include "nrf_log.h"
-#include "nrf_log_ctrl.h"
-#include "nrf_log_default_backends.h"
+// #include "nrf_log.h" TODO: Platform log
+// #include "nrf_log_ctrl.h"
+// #include "nrf_log_default_backends.h"
 
 #define SPI_INSTANCE  BOARD_SPI_INSTANCE /**< SPI instance index. */
+
+#if (BOARD_SPI_FREQUENCY == RUUVI_SPI_FREQ_1M)
+  #define SPI_FREQUENCY NRF_DRV_SPI_FREQ_1M
+#elif (BOARD_SPI_FREQUENCY == RUUVI_SPI_FREQ_8M)
+  #define SPI_FREQUENCY NRF_DRV_SPI_FREQ_8M
+#else
+  #define SPI_FREQUENCY SPI0_DEFAULT_FREQUENCY
+#endif
+
 static const nrf_drv_spi_t spi = NRF_DRV_SPI_INSTANCE(SPI_INSTANCE);  /**< SPI instance. */
-static volatile bool spi_xfer_done = false;  /**< Flag used to indicate that SPI instance completed the transfer. */
+static volatile bool spi_xfer_done = true;  /**< Flag used to indicate that SPI instance completed the transfer. */
 static volatile bool spi_init_done = false;  /**< Flag used to indicate that SPI instance is initialized. */
 
 /**
@@ -67,7 +77,7 @@ static void spi_event_handler(nrf_drv_spi_evt_t const * p_event,
   void *                    p_context)
 {
   spi_xfer_done = true;
-  NRF_LOG_INFO("Transfer completed.");
+  // NRF_LOG_INFO("Transfer completed.");
 }
 
 /**
@@ -87,7 +97,7 @@ ruuvi_status_t spi_init(void)
   spi_config.sck_pin      = SPIM0_SCK_PIN;
   spi_config.irq_priority = SPI_DEFAULT_CONFIG_IRQ_PRIORITY;
   spi_config.orc          = 0xFF;       
-  spi_config.frequency    = NRF_DRV_SPI_FREQ_8M;                     
+  spi_config.frequency    = SPI_FREQUENCY;                     
   spi_config.mode         = NRF_DRV_SPI_MODE_0;                      
   spi_config.bit_order    = NRF_DRV_SPI_BIT_ORDER_MSB_FIRST;         
   err_code = nrf_drv_spi_init(&spi, &spi_config, spi_event_handler, NULL);
@@ -102,6 +112,7 @@ ruuvi_status_t spi_init(void)
   nrf_gpio_pin_set(SPIM0_SS_ACCELERATION_PIN);
 
   if(NRF_SUCCESS == err_code) { spi_init_done = true; }
+  // NRF_LOG_INFO("SPI INIT completed.");
   return PLATFORM_TO_RUUVI_ERROR(&err_code);
 }
 
@@ -129,6 +140,7 @@ int8_t spi_bosch_platform_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *data,
   //Return error if not init or if busy
   if(!spi_init_done) { return NRF_ERROR_INVALID_STATE; }
   if(!spi_xfer_done) { return NRF_ERROR_BUSY; }
+  // NRF_LOG_INFO("Start Bosch transfer.");
 
   //Lock driver
   spi_xfer_done = false;
@@ -153,6 +165,7 @@ int8_t spi_bosch_platform_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *data,
   nrf_gpio_pin_set(dev_id);
 
   APP_ERROR_CHECK(err_code);
+  // NRF_LOG_INFO("Bosch transfer completed. %x %x", spi_init_done, spi_xfer_done);
   return (NRF_SUCCESS == err_code) ? 0 : -1;
 }
 
@@ -162,8 +175,10 @@ int8_t spi_bosch_platform_write(uint8_t dev_id, uint8_t reg_addr, uint8_t *data,
 int8_t spi_bosch_platform_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *data, uint16_t len)
 {
   //Return error if not init or if busy
+  // NRF_LOG_INFO("Enter Bosch read. %x %x", spi_init_done, spi_xfer_done);
   if(!spi_init_done) { return NRF_ERROR_INVALID_STATE; }
   if(!spi_xfer_done) { return NRF_ERROR_BUSY; }
+  // NRF_LOG_INFO("Start Bosch read.");
 
   //Lock driver
   spi_xfer_done = false;
@@ -171,21 +186,28 @@ int8_t spi_bosch_platform_read(uint8_t dev_id, uint8_t reg_addr, uint8_t *data, 
   int8_t err_code = NRF_SUCCESS;
 
   nrf_gpio_pin_clear(dev_id);
+  uint8_t* p_write = calloc(len+1, sizeof(uint8_t));
+  uint8_t* p_read  = calloc(len+1, sizeof(uint8_t));
+  p_write[0] = reg_addr;
   //TX address
-  err_code |= nrf_drv_spi_transfer(&spi, &reg_addr, 1, NULL, 0);
+  /*err_code |= nrf_drv_spi_transfer(&spi, &reg_addr, 1, NULL, 0);
   while (!spi_xfer_done)
   {
     err_code |= platform_yield();
   }
   //Read data
-  spi_xfer_done = false;
-  err_code |= nrf_drv_spi_transfer(&spi, NULL, 0, data, len);
+  spi_xfer_done = false;*/
+  err_code |= nrf_drv_spi_transfer(&spi, p_write, len+1, p_read, len+1);
   while (!spi_xfer_done)
   {
     err_code |= platform_yield();
   }
+  memcpy(data, p_read+1, len);
   nrf_gpio_pin_set(dev_id);
 
+
+  // NRF_LOG_INFO("Bosch read completed.");
+ 
   APP_ERROR_CHECK(err_code);
   return (NRF_SUCCESS == err_code) ? 0 : -1;
 }
@@ -207,19 +229,25 @@ int32_t spi_stm_platform_write(void* dev_id, uint8_t reg_addr, uint8_t *data,
   uint8_t ss = *(uint8_t*)dev_id;
 
   nrf_gpio_pin_clear(ss);
+
+  uint8_t* p_write = malloc(len+1);
   //TX address
-  err_code |= nrf_drv_spi_transfer(&spi, &reg_addr, 1, NULL, 0);
+  // err_code |= nrf_drv_spi_transfer(&spi, &reg_addr, 1, NULL, 0);
+  // while (!spi_xfer_done)
+  // {
+  //   err_code |= platform_yield();
+  // }
+  // //Write data
+  // spi_xfer_done = false;
+
+  p_write[0] = reg_addr;
+
+  err_code |= nrf_drv_spi_transfer(&spi, p_write, len+1, NULL, 0);
   while (!spi_xfer_done)
   {
     err_code |= platform_yield();
   }
-  //Write data
-  spi_xfer_done = false;
-  err_code |= nrf_drv_spi_transfer(&spi, data, len, NULL, 0);
-  while (!spi_xfer_done)
-  {
-    err_code |= platform_yield();
-  }
+  free(p_write);
   nrf_gpio_pin_set(ss);
 
   APP_ERROR_CHECK(err_code);
