9035cc73bddfd6b3a27e472f87af86a2004fde4e
Otso Jousimaa <otso@ojousima.net>
2018-09-26 20:08:46 +0300

    Add event handler

--- interfaces/communication/ruuvi_interface_communication.h
+++ interfaces/communication/ruuvi_interface_communication.h
@@ -19,18 +19,27 @@ typedef struct{
   bool repeat;
 }ruuvi_interface_communication_message_t;
 
+typedef enum {
+  RUUVI_INTERFACE_COMMUNICATION_CONNECTED,
+  RUUVI_INTERFACE_COMMUNICATION_DISCONNECTED,
+  RUUVI_INTERFACE_COMMUNICATION_SENT,
+  RUUVI_INTERFACE_COMMUNICATION_RECEIVED,
+}ruuvi_interface_communication_evt_t;
+
 typedef struct ruuvi_interface_communication_t ruuvi_interface_communication_t;          // forward declaration *and* typedef
 typedef ruuvi_driver_status_t(*ruuvi_interface_communication_xfer_fp_t)(ruuvi_interface_communication_message_t*);
 typedef ruuvi_driver_status_t(*ruuvi_interface_communication_init_fp_t)(ruuvi_interface_communication_t* const);
+typedef ruuvi_driver_status_t(*ruuvi_interface_communication_evt_handler_fp_t)(const ruuvi_interface_communication_evt_t);
 
 // Every Ruuvi communication channel must  be able to send data and receive data.
 // Channels can be init or uninit
 struct ruuvi_interface_communication_t
 {
-  ruuvi_interface_communication_xfer_fp_t send;
-  ruuvi_interface_communication_xfer_fp_t read;
-  ruuvi_interface_communication_init_fp_t init;
-  ruuvi_interface_communication_init_fp_t uninit;
+  ruuvi_interface_communication_xfer_fp_t        send;
+  ruuvi_interface_communication_xfer_fp_t        read;
+  ruuvi_interface_communication_init_fp_t        init;
+  ruuvi_interface_communication_init_fp_t        uninit;
+  ruuvi_interface_communication_evt_handler_fp_t on_evt;
 };
 
 #endif
