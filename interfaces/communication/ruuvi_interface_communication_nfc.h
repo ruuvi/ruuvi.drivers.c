642e6316fae6b16863ddf34b3b46c203b2728b88
mdxs <van.wijgerden@mdxs.net>
2019-03-04 15:54:51 +0100

    Fixed comments in NFC interface header
    Fixed some typos and made other comments more consistent in NFC interface header.

--- interfaces/communication/ruuvi_interface_communication_nfc.h
+++ interfaces/communication/ruuvi_interface_communication_nfc.h
@@ -1,5 +1,5 @@
 /**
- * Ruuvi NFC interface
+ * Ruuvi NFC interface.
  *
  * License: BSD-3
  * Author: Otso Jousimaa <otso@ojousima.net>
@@ -11,16 +11,16 @@
 #include <stdint.h>
 
 /*
- * Initializes NFC hardware
+ * Initializes NFC hardware.
  *
- * Returns RUUVI_DIRVER_SUCCESS on success, RUUVI_DIRVER_ERROR_INVALID_STATE if radio is already initialized
+ * Returns RUUVI_DRIVER_SUCCESS on success, RUUVI_DRIVER_ERROR_INVALID_STATE if radio is already initialized
  */
 ruuvi_driver_status_t ruuvi_interface_communication_nfc_init(ruuvi_interface_communication_t* const channel);
 
 /*
- * Uninitializes NFC hardware
+ * Uninitializes NFC hardware.
  *
- * Returns RUUVI_DIRVER_SUCCESS on success or if radio was not initialized.
+ * Returns RUUVI_DRIVER_SUCCESS on success or if radio was not initialized.
  * Returns RUUVI_DRIVER_ERROR_INVALID_STATE if radio hardware was initialized by another radio module.
  */
 ruuvi_driver_status_t ruuvi_interface_communication_nfc_uninit(ruuvi_interface_communication_t* const channel);
@@ -39,7 +39,7 @@ ruuvi_driver_status_t ruuvi_interface_communication_nfc_send(ruuvi_interface_com
 /**
  * Sets the device firmware version into "FW" text field.
  *
- *  parameter version: Pointer on string representation of the version. ie. "FW: ruuvi.firmware.c 3.10.0"
+ *  parameter version: Pointer on string representation of the version. ie. "FW: RuuviFW 3.17.0"
  *
  * returns RUUVI_DRIVER_SUCCESS on success
  * returns RUUVI_DRIVER_ERROR_NULL if version is NULL and length != 0
@@ -50,6 +50,8 @@ ruuvi_driver_status_t ruuvi_interface_communication_nfc_fw_version_set(const uin
 /**
  * Sets the device mac address into "ad" text field.
  *
+ *  parameter address: Pointer on string representation of the mac address. ie. "MAC: 12:34:56:78:90:AB"
+ *
  * returns RUUVI_DRIVER_SUCCESS on success
  * returns RUUVI_DRIVER_ERROR_NULL if address is NULL and length != 0
  * returns RUUVI_DRIVER_INVALID_LENGTH if name is over 32 bytes long
@@ -57,9 +59,9 @@ ruuvi_driver_status_t ruuvi_interface_communication_nfc_fw_version_set(const uin
 ruuvi_driver_status_t ruuvi_interface_communication_nfc_address_set(const uint8_t* const address, const uint8_t length);
 
 /**
- * Sets the device id into "id" text field. Set NULL/0 to disable
+ * Sets the device id into "id" text field. Set NULL/0 to disable.
  *
- *  parameter id: Pointer on string representation of the id. ie. "ID: 12:34:56:78:90:AB:CD:EF
+ *  parameter id: Pointer on string representation of the id. ie. "ID: 12:34:56:78:90:AB:CD:EF"
  *
  * returns RUUVI_DRIVER_SUCCESS on success
  * returns RUUVI_DRIVER_ERROR_NULL if id is NULL and length != 0
