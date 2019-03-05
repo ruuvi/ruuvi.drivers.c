9fa5a4c65a1298815810b06817b646f267bf2dfb
Otso Jousimaa <otso@ojousima.net>
2019-03-05 22:54:43 +0200

    Add I2C support, add BME280 over I2C

---
+++ interfaces/i2c/ruuvi_interface_i2c_bme280.c
@@ -0,0 +1,75 @@
+#include "ruuvi_driver_enabled_modules.h"
+#if RUUVI_INTERFACE_ENVIRONMENTAL_BME280_ENABLED
+/**
+ * @addtogroup I2C
+ *
+ */
+/*@{*/
+/**
+ * @file ruuvi_interface_i2c_bme280.c
+ * @author Otso Jousimaa <otso@ojousima.net>
+ * @date 2019-03-04
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ * @brief Wrapper for BME280 I2C calls
+ *
+ */
+#include <stdint.h>
+#include <string.h>
+
+#include "ruuvi_boards.h"
+#include "ruuvi_driver_error.h"
+#include "ruuvi_interface_gpio.h"
+#include "ruuvi_interface_i2c.h"
+#include "ruuvi_interface_yield.h"
+
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Write      | (reg_data[0])       |
+     * | Write      | (....)              |
+     * | Write      | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+int8_t ruuvi_interface_i2c_bme280_write(uint8_t dev_id, uint8_t reg_addr,
+                                        uint8_t* reg_data, uint16_t len)
+{
+  ruuvi_driver_status_t err_code = RUUVI_DRIVER_SUCCESS;
+  // Support only 1-byte writes
+  if(1 > len || 2 < len) { return -1; }
+  uint8_t wbuf[2] = {0};
+  wbuf[0] = reg_addr;
+  wbuf[1] = reg_data[0];
+  err_code |= ruuvi_interface_i2c_write_blocking(dev_id, wbuf, 2, true);
+  return (RUUVI_DRIVER_SUCCESS == err_code) ? 0 : -1;
+}
+
+    /*
+     * Data on the bus should be like
+     * |------------+---------------------|
+     * | I2C action | Data                |
+     * |------------+---------------------|
+     * | Start      | -                   |
+     * | Write      | (reg_addr)          |
+     * | Stop       | -                   |
+     * | Start      | -                   |
+     * | Read       | (reg_data[0])       |
+     * | Read       | (....)              |
+     * | Read       | (reg_data[len - 1]) |
+     * | Stop       | -                   |
+     * |------------+---------------------|
+     */
+
+int8_t ruuvi_interface_i2c_bme280_read(uint8_t dev_id, uint8_t reg_addr,
+                                       uint8_t* reg_data, uint16_t len)
+{
+  ruuvi_driver_status_t err_code = RUUVI_DRIVER_SUCCESS;
+  err_code |= ruuvi_interface_i2c_write_blocking(dev_id, &reg_addr, 1, true);
+  err_code |= ruuvi_interface_i2c_read_blocking(dev_id, reg_data, len);
+  return (RUUVI_DRIVER_SUCCESS == err_code) ? 0 : -1;
+}
+#endif
---
+++ interfaces/i2c/ruuvi_interface_i2c_bme280.h
@@ -0,0 +1,69 @@
+#ifndef RUUVI_INTERFACE_I2C_BME280_H
+#define RUUVI_INTERFACE_I2C_BME280_H
+#include "ruuvi_driver_error.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+/**
+ * @addtogroup I2C
+ * @{
+ */
+/**
+ * @file ruuvi_interface_i2c_bme280.h
+ * @author Otso Jousimaa <otso@ojousima.net>
+ * @brief SPI read/write functions for Bosch BME280.
+ * @date 2019-03-04
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ *
+ * You'll need to get the official Bosch BME280 driver, available on
+ * <a href="https://github.com/BoschSensortec/BME280_driver">GitHub</a>.
+ * The wrappers will use Ruuvi Interface internally, so you don't have to port these to
+ * use BME280 on a new platform. You're required to port @ref Yield, @ref GPIO and @ref I2C.
+ *
+ * General usage is:
+ * @code{c}
+ * static struct bme280_dev dev = {0};
+ * dev.dev_id = bme280_cs_pin;
+ * dev.intf = BME280_I2C_INTF;
+ * dev.read = ruuvi_interface_i2c_bme280_read;
+ * dev.write = ruuvi_interface_i2c_bme280_write;
+ * dev.delay_ms = bosch_delay_ms;
+ * bme280_init(&dev);
+ * @endcode
+ */
+
+/**
+ * @brief I2C write function for BME280
+ *
+ * Binds Ruuvi Interface I2C functions into official Bosch BME280 driver.
+ *
+ * @param[in] dev_id @ref I2C interface handle, i.e. I2C addess of BME280.
+ * @param[in] reg_addr BME280 register address to write.
+ * @param[in] p_reg_data pointer to data to be written.
+ * @param[in] len length of data to be written.
+ * @warning I2C address depends on BME280 SDO logic level
+ * @return negative number on error
+ * @return 0 on success
+ * @return positive number on warning
+ **/
+int8_t ruuvi_interface_i2c_bme280_write(uint8_t dev_id, uint8_t reg_addr,
+                                        uint8_t* p_reg_data, uint16_t len);
+
+/**
+ * @brief I2C Read function for BME280
+ *
+ * Binds Ruuvi Interface I2C functions into official Bosch BME280 driver.
+ *
+ * @param[in] dev_id @ref I2C interface handle, i.e. I2C addess of BME280.
+ * @param[in] reg_addr BME280 register address to read.
+ * @param[in] p_reg_data pointer to data to be received.
+ * @param[in] len length of data to be received.
+ * @warning I2C address depends on BME280 SDO logic level
+ * @return negative number on error
+ * @return 0 on success
+ * @return positive number on warning
+ **/
+int8_t ruuvi_interface_i2c_bme280_read(uint8_t dev_id, uint8_t reg_addr,
+                                       uint8_t* reg_data, uint16_t len);
+/** @} */
+#endif
