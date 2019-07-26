a2ffd0b752bc2e99b5ee7aa4949d1b9247090fa4
Otso Jousimaa <otso@ojousima.net>
2019-07-26 19:46:39 +0300

    Increase timeout, remove extra volatile ops

--- nrf5_sdk15_platform/i2c/ruuvi_nrf5_sdk15_i2c.c
+++ nrf5_sdk15_platform/i2c/ruuvi_nrf5_sdk15_i2c.c
@@ -82,14 +82,14 @@ static void byte_timeout_set(const
   switch(freq)
   {
     case RUUVI_INTERFACE_I2C_FREQUENCY_100k:
-      timeout_us_per_byte = 400;
+      timeout_us_per_byte = 1000;
 
     case RUUVI_INTERFACE_I2C_FREQUENCY_250k:
-      timeout_us_per_byte = 200;
+      timeout_us_per_byte = 400;
 
     case RUUVI_INTERFACE_I2C_FREQUENCY_400k:
     default:
-      timeout_us_per_byte = 100;
+      timeout_us_per_byte = 200;
   }
 }
 
@@ -168,9 +168,6 @@ ruuvi_driver_status_t ruuvi_interface_i2c_write_blocking(const uint8_t address,
   if(m_tx_in_progress) { return RUUVI_DRIVER_ERROR_BUSY; }
 
   int32_t err_code = NRF_SUCCESS;
-  // Size optimization breaks the DMA-callback structure somehow. This
-  // disables the size optimization. 
-  uint8_t volatile * do_not_optimize_this_please = p_tx;
   m_tx_in_progress = true;
   err_code |= nrf_drv_twi_tx(&m_twi, address, p_tx, tx_len, !stop);
   volatile uint32_t timeout = 0;
@@ -182,7 +179,6 @@ ruuvi_driver_status_t ruuvi_interface_i2c_write_blocking(const uint8_t address,
   if(timeout >= (timeout_us_per_byte * tx_len)) { err_code |= NRF_ERROR_TIMEOUT; }
   err_code |= xfer_status;
   xfer_status = NRF_SUCCESS;
-  timeout += do_not_optimize_this_please[0];
   return ruuvi_nrf5_sdk15_to_ruuvi_error(err_code);
 }
 
@@ -202,9 +198,6 @@ ruuvi_driver_status_t ruuvi_interface_i2c_read_blocking(const uint8_t address,
   if(NULL == p_rx) { return RUUVI_DRIVER_ERROR_NULL; }
 
   int32_t err_code = NRF_SUCCESS;
-  // Size optimization breaks the DMA-callback structure somehow. This
-  // disables the size optimization. 
-  uint8_t volatile * do_not_optimize_this_please = p_rx;
   m_tx_in_progress = true;
   err_code |= nrf_drv_twi_rx(&m_twi, address, p_rx, rx_len);
   volatile uint32_t timeout = 0;
@@ -216,7 +209,6 @@ ruuvi_driver_status_t ruuvi_interface_i2c_read_blocking(const uint8_t address,
   if(timeout >= (timeout_us_per_byte * rx_len)) { err_code |= NRF_ERROR_TIMEOUT; }
   err_code |= xfer_status;
   xfer_status = NRF_SUCCESS;
-  timeout += do_not_optimize_this_please[0];
   return ruuvi_nrf5_sdk15_to_ruuvi_error(err_code);
 }
 
