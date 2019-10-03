66871f57bf2bf46d77475c5e23836edbe9c4ae21
Otso Jousimaa <otso@ojousima.net>
2019-10-03 17:35:55 +0300

    Add UART stub

---
+++ interfaces/uart/ruuvi_interface_uart.h
@@ -0,0 +1,102 @@
+#ifndef RUUVI_INTERFACE_UART_H
+#define RUUVI_INTERFACE_UART_H
+#include "ruuvi_driver_error.h"
+#include "ruuvi_interface_gpio.h"
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+/**
+ * @defgroup UART UART functions
+ * @brief Functions for using UART bus
+ *
+ */
+/*@{*/
+/**
+ * @file ruuvi_interface_uart.h
+* @brief Interface for UART operations
+ * @author Otso Jousimaa <otso@ojousima.net>
+ * @date 2019-10-01
+ * @copyright Ruuvi Innovations Ltd, license BSD-3-Clause.
+ *
+ */
+
+/**
+ * Baudrate for UART
+ */
+typedef enum
+{
+  RUUVI_INTERFACE_UART_BAUD_9600,   //!< 9600 bauds
+  RUUVI_INTERFACE_UART_BAUD_115200 //!< 2 Mbps
+} ruuvi_interface_uart_baud_t;
+
+/**
+ * Configuration for initializing UART
+ */
+typedef struct
+{
+  ruuvi_interface_gpio_id_t tx;     //!< pin number of TX.
+  ruuvi_interface_gpio_id_t rx;     //!< pin number of RX.
+  ruuvi_interface_gpio_id_t cts;    //!< pin number of CTS.
+  ruuvi_interface_gpio_id_t rts;    //!< pin number of RTS.
+  ruuvi_interface_uart_baud_t baud; //!< Baud rate, see @ref ruuvi_interface_uart_baud_t.
+  bool parity;                      //!< True -> parity bit enabled. False -> disabled.
+  bool hwfc;                        //!< True -> Hardware flow control enabled. False -> disabled. 
+} ruuvi_interface_uart_init_config_t;
+
+/**
+ * @brief Callback function for received data.
+ */
+//typedef void (*ruuvi_interface_uart_cb_t)(const uint8_t* const data, const size_t data_length);
+
+/**
+ * @brief Initialize UART driver with given settings
+ *
+ * This function also handles configuring the GPIO pins as required
+ *
+ * @param config Configuration of the UART peripheral. Will setup given pins as required.
+ * @return RUUVI_DRIVER_SUCCESS if no error occurred
+ * @return RUUVI_DRIVER_ERROR_INVALID_STATE if UART was already initialized
+ **/
+ruuvi_driver_status_t ruuvi_interface_uart_init(const ruuvi_interface_uart_init_config_t*
+    const config);
+
+/**
+ * @brief check if UART interface is already initialized.
+ * 
+ * @return @c true if UART is initialized
+ * @return @c false otherwise
+ */
+bool ruuvi_interface_uart_is_init();
+
+/**
+ * @brief Uninitialize UART driver.
+ * 
+ * This function might not uninitialize the UART GPIO pins, only the underlying peripheral.
+ * Uninitialized GPIOs explicitly if that is required.
+ *
+ * @return RUUVI_DRIVER_SUCCESS
+ * @warning Uninitializes the UART peripheral, may or may not uninitialize the associated gpio pins
+ **/
+ruuvi_driver_status_t ruuvi_interface_uart_uninit();
+
+/**
+ * @brief UART write function.
+ * Function is blocking and will not sleep while transaction is ongoing. 
+ *
+ * @param p_tx pointer to data to be sent. Must be in RAM. 
+ * @param tx_len length of data to be sent.
+ * @return RUUVI_DRIVER_SUCCSS when data was sent.
+ * @return RUUVI_DRIVER_ERROR_NULL if p_tx is NULL.
+ * @return RUUVI_DRIVER_ERROR_INVALID_ADDRESS if p_tx is not in RAM
+ * @return error code from stack on other error.
+ **/
+ruuvi_driver_status_t ruuvi_interface_uart_send_blocking(const uint8_t* const p_tx,
+    const size_t tx_len);
+
+/**
+ * @brief Configure a callback to be called once data is received. 
+ */
+//void ruuvi_interface_uart_rx_cb_set(const ruuvi_interface_uart_cb_t cb);
+/* @} */
+#endif
---
+++ nrf5_sdk15_platform/uart/ruuvi_nrf5_sdk15_uart.c
@@ -0,0 +1,100 @@
+#include "ruuvi_driver_enabled_modules.h"
+#if RUUVI_NRF5_SDK15_UART_ENABLED
+#include "ruuvi_driver_error.h"
+#include "ruuvi_interface_uart.h"
+#include "ruuvi_interface_log.h"
+#include "ruuvi_interface_yield.h"
+#include "ruuvi_nrf5_sdk15_error.h"
+#include "ruuvi_nrf5_sdk15_gpio.h"
+#include "nrfx_uarte.h"
+
+#ifndef RUUVI_NRF5_SDK15_UART_LOG_LEVEL
+  #define LOG_LEVEL RUUVI_INTERFACE_LOG_INFO
+#else
+  #define LOG_LEVEL RUUVI_NRF5_SDK15_UART_LOG_LEVEL
+#endif
+#define LOG(msg)  (ruuvi_interface_log(LOG_LEVEL, msg))
+#define LOGD(msg)  (ruuvi_interface_log(RUUVI_INTERFACE_LOG_DEBUG, msg))
+
+static bool m_uart_is_init = false;
+static nrfx_uarte_t m_uart = NRFX_UARTE_INSTANCE(0);
+
+// Convert constants from Ruuvi to nRF52 SDK
+static nrf_uarte_baudrate_t ruuvi_to_nrf_baudrate(const ruuvi_interface_uart_baud_t baud)
+{
+  switch(baud)
+  {
+    case RUUVI_INTERFACE_UART_BAUD_9600:
+      return NRF_UARTE_BAUDRATE_9600;
+
+    case RUUVI_INTERFACE_UART_BAUD_115200:
+    default:
+      return NRF_UARTE_BAUDRATE_115200;
+  }
+}
+
+// Handle UART events
+static void uart_handler(nrfx_uarte_event_t const *p_event, void *p_context)
+{
+  switch(p_event->type)
+  {
+    case NRFX_UARTE_EVT_TX_DONE: ///< Requested TX transfer completed.   
+      LOG("TX\r\n");
+      break;
+    case NRFX_UARTE_EVT_RX_DONE: ///< Requested RX transfer completed.
+      LOG("RX\r\n");
+      break;
+    case NRFX_UARTE_EVT_ERROR:   ///< Error reported by UART peripheral.
+      LOG("!\r\n");
+      break;
+    default:
+      LOG("?\r\n");
+      break;
+  }
+}
+
+ruuvi_driver_status_t ruuvi_interface_uart_init(const ruuvi_interface_uart_init_config_t*
+    const config)
+{
+  nrfx_err_t nrf_status = NRF_SUCCESS;
+  nrfx_uarte_config_t nrf_config = NRFX_UARTE_DEFAULT_CONFIG;
+  nrf_config.baudrate = ruuvi_to_nrf_baudrate(config->baud);
+  nrf_config.pselcts = ruuvi_to_nrf_pin_map(config->cts);
+  nrf_config.pselcts = ruuvi_to_nrf_pin_map(config->rts);
+  nrf_config.pselcts = ruuvi_to_nrf_pin_map(config->tx);
+  nrf_config.pselcts = ruuvi_to_nrf_pin_map(config->rx);
+  nrf_config.parity  = config->parity ? NRF_UARTE_PARITY_INCLUDED : NRF_UARTE_PARITY_EXCLUDED;
+  nrf_config.hwfc    = config->hwfc ? NRF_UARTE_HWFC_ENABLED : NRF_UARTE_HWFC_DISABLED;
+
+  nrf_status |= nrfx_uarte_init(&m_uart, &nrf_config, uart_handler);
+  if(NRF_SUCCESS == nrf_status)
+  {
+    m_uart_is_init = true;
+  }
+  
+  return ruuvi_nrf5_sdk15_to_ruuvi_error(nrf_status);
+
+}
+
+bool ruuvi_interface_uart_is_init()
+{
+  return m_uart_is_init;
+}
+
+ruuvi_driver_status_t ruuvi_interface_uart_uninit()
+{
+  nrfx_uarte_uninit(&m_uart);
+  return RUUVI_DRIVER_SUCCESS;
+}
+
+ruuvi_driver_status_t ruuvi_interface_uart_send_blocking(const uint8_t* const p_tx,
+    const size_t tx_len)
+{
+  if(NULL == p_tx) { return RUUVI_DRIVER_ERROR_NULL; }
+  nrfx_err_t err_code = NRF_SUCCESS;
+  err_code |= nrfx_uarte_tx(&m_uart, p_tx, tx_len);
+  while(NRF_SUCCESS == err_code && nrfx_uarte_tx_in_progress(&m_uart));
+  return err_code;
+}
+
+#endif
