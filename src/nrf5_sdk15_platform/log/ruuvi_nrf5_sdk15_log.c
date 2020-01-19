44dc3e4f03d6cff1b35c7bc5989cc1102f0740e7
Otso Jousimaa <otso@ojousima.net>
2020-01-19 21:50:49 +0200

    Match refactored naming conventions

--- src/nrf5_sdk15_platform/log/ruuvi_nrf5_sdk15_log.c
+++ src/nrf5_sdk15_platform/log/ruuvi_nrf5_sdk15_log.c
@@ -27,31 +27,31 @@
 #include "nrf_log_ctrl.h"
 NRF_LOG_MODULE_REGISTER();
 
-static ruuvi_interface_log_severity_t log_level;
-ruuvi_driver_status_t ruuvi_interface_log_init(const ruuvi_interface_log_severity_t
+static ri_log_severity_t log_level;
+rd_status_t ri_log_init(const ri_log_severity_t
     min_severity)
 {
   log_level = min_severity;
   NRF_LOG_INIT(NULL);
   NRF_LOG_DEFAULT_BACKENDS_INIT();
-  return RUUVI_DRIVER_SUCCESS;
+  return RD_SUCCESS;
 }
 
-ruuvi_driver_status_t ruuvi_interface_log_flush(void)
+rd_status_t ri_log_flush(void)
 {
   NRF_LOG_FLUSH();
   // Use microsecond delay to avoid getting stuck in timer loop if
   // logs are flushed in interrupt context.
-  ruuvi_interface_delay_us(5000);
-  return RUUVI_DRIVER_SUCCESS;
+  ri_delay_us(5000);
+  return RD_SUCCESS;
 }
 
-void ruuvi_interface_log(const ruuvi_interface_log_severity_t severity,
+void ri_log(const ri_log_severity_t severity,
                          const char* const message)
 {
   if(NULL == message)
   {
-    RUUVI_DRIVER_ERROR_CHECK(RUUVI_DRIVER_ERROR_NULL, RUUVI_DRIVER_ERROR_NULL);
+    RD_ERROR_CHECK(RD_ERROR_NULL, RD_ERROR_NULL);
     return;
   }
 
