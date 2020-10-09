f47dda7c79a071ca0992429c2b7b6ddfa8e2486c
Otso Jousimaa <otso@ojousima.net>
2020-10-09 12:35:48 +0300

    Document as stable

--- src/ruuvi_driver_enabled_modules.h
+++ src/ruuvi_driver_enabled_modules.h
@@ -18,7 +18,7 @@
 #define RUUVI_DRIVER_ENABLED_MODULES_H
 
 /** @brief SemVer string, must match latest tag. */
-#define RUUVI_DRIVERS_SEMVER "0.3.3"
+#define RUUVI_DRIVERS_SEMVER "3.4.0"
 
 #ifdef CEEDLING
 #  define ENABLE_DEFAULT 1
