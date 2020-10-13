c0eec6828c9f80e86a3f51f3d26a9bbf45488066
Otso Jousimaa <otso@ojousima.net>
2020-10-13 19:37:55 +0300

    Update changelog for 3.4.2

--- src/ruuvi_driver_enabled_modules.h
+++ src/ruuvi_driver_enabled_modules.h
@@ -18,7 +18,7 @@
 #define RUUVI_DRIVER_ENABLED_MODULES_H
 
 /** @brief SemVer string, must match latest tag. */
-#define RUUVI_DRIVERS_SEMVER "3.4.1"
+#define RUUVI_DRIVERS_SEMVER "3.4.2"
 
 #ifdef CEEDLING
 #  define ENABLE_DEFAULT 1
