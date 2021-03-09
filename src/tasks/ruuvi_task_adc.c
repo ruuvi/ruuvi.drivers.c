2c4137da248580f23adca8a654466d5dab99918d
niksiboxi <niksiboxi@hotmail.com>
2021-03-09 15:43:05 +0200

    Uninitialize ADC on error

--- src/tasks/ruuvi_task_adc.c
+++ src/tasks/ruuvi_task_adc.c
@@ -309,16 +309,16 @@ rd_status_t rt_adc_vdd_prepare (rd_sensor_configuration_t * const vdd_adc_config
 {
     rd_status_t err_code = RD_SUCCESS;
     err_code |= rt_adc_init();
-    err_code |= rt_adc_configure_se (vdd_adc_configuration, RI_ADC_GND,
+    err_code |= rt_adc_configure_se (vdd_adc_configuration, RI_ADC_AINVDD,
                                      ABSOLUTE);
 
-    if (err_code == RD_ERROR_INVALID_PARAM)
+    if (err_code == RD_SUCCESS)
     {
-        err_code == rt_adc_uninit();
+        m_vdd_prepared = true;
     }
     else
     {
-        m_vdd_prepared = (RD_SUCCESS == err_code);
+        (void) rt_adc_uninit();
     }
 
     return err_code;
