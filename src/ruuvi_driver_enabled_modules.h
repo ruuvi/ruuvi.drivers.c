ae924311390021afb6ba5519138af1714b5846bf
Otso Jousimaa <otso@ojousima.net>
2021-11-30 18:30:51 +0200

    Update version patch, changelog

--- src/ruuvi_driver_enabled_modules.h
+++ src/ruuvi_driver_enabled_modules.h
@@ -18,7 +18,7 @@
 #define RUUVI_DRIVER_ENABLED_MODULES_H
 
 /** @brief SemVer string, must match latest tag. */
-#define RUUVI_DRIVERS_SEMVER "3.9.2"
+#define RUUVI_DRIVERS_SEMVER "3.9.3"
 
 #ifdef CEEDLING
 #  define ENABLE_DEFAULT 1
