c4f9d7d44dec017e352fc1e55b4c87c1c4b3826f
TheSomeMan <AlexeyN.Skvortsov@gmail.com>
2023-05-22 23:19:20 +0700

    Update sensirion_i2c_hal.c

--- embedded-i2c-sen5x/sensirion_i2c_hal.c
+++ embedded-i2c-sen5x/sensirion_i2c_hal.c
@@ -29,9 +29,53 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+#include "nrfx_twi.h"
+#include "nrf_delay.h"
+#include "nrf_gpio.h"
+
 #include "sensirion_i2c_hal.h"
 #include "sensirion_common.h"
 #include "sensirion_config.h"
+
+#define TWI_INSTANCE_ID     1
+
+
+/* TWIM instance. */
+static const nrfx_twi_t m_twi = NRFX_TWI_INSTANCE(TWI_INSTANCE_ID);
+
+
+void twim_handler(nrfx_twi_evt_t const * p_event, void * p_context)
+{
+    // Handler for TWI events.
+    switch (p_event->type)
+    {
+      case NRFX_TWI_EVT_DONE:
+        break;
+      case NRFX_TWI_EVT_ADDRESS_NACK:
+        break;
+      case NRFX_TWI_EVT_DATA_NACK:
+        break;
+    }
+}
+
+void twi_init (void)
+{
+    ret_code_t err_code;
+
+    const nrfx_twi_config_t twi_config = {
+       .scl                = RB_I2C_SCL_PIN,
+       .sda                = RB_I2C_SDA_PIN,
+       .frequency          = NRF_TWI_FREQ_100K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .hold_bus_uninit     = false
+    };
+
+    err_code = nrfx_twi_init(&m_twi, &twi_config, NULL /*&twim_handler*/, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrfx_twi_enable(&m_twi);
+}
 
 /*
  * INSTRUCTIONS
@@ -52,10 +96,7 @@
  * @returns         0 on success, an error code otherwise
  */
 int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx) {
-    /* TODO:IMPLEMENT or leave empty if all sensors are located on one single
-     * bus
-     */
-    return NOT_IMPLEMENTED_ERROR;
+    return 0;
 }
 
 /**
@@ -63,7 +104,11 @@ int16_t sensirion_i2c_hal_select_bus(uint8_t bus_idx) {
  * communication.
  */
 void sensirion_i2c_hal_init(void) {
-    /* TODO:IMPLEMENT */
+#if 0
+  twi_init();
+  nrf_gpio_cfg_sense_input(RB_I2C_SCL_PIN, NRF_GPIO_PIN_PULLUP, NRF_GPIO_PIN_NOSENSE);
+  nrf_gpio_cfg_sense_input(RB_I2C_SDA_PIN, NRF_GPIO_PIN_PULLUP, NRF_GPIO_PIN_NOSENSE);
+#endif
 }
 
 /**
@@ -84,8 +129,27 @@ void sensirion_i2c_hal_free(void) {
  * @returns 0 on success, error code otherwise
  */
 int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint16_t count) {
-    /* TODO:IMPLEMENT */
-    return NOT_IMPLEMENTED_ERROR;
+#if 1
+    nrfx_twi_xfer_desc_t xfer_desc;
+    xfer_desc.address = address;
+    xfer_desc.type = NRFX_TWI_XFER_RX;
+    xfer_desc.primary_length = count;
+    xfer_desc.p_primary_buf = data;
+
+    ret_code_t err_code = nrfx_twi_xfer(&m_twi, &xfer_desc, 0);
+    if (NRF_SUCCESS != err_code) {
+      return err_code;
+    }
+    while (nrfx_twi_is_busy(&m_twi)) {
+    }
+#else
+    nrfx_err_t err_code = nrfx_twi_rx(&m_twi, address, data, count);
+    if (NRF_SUCCESS != err_code) {
+        return err_code;
+    }
+#endif
+
+    return NO_ERROR;
 }
 
 /**
@@ -101,8 +165,27 @@ int8_t sensirion_i2c_hal_read(uint8_t address, uint8_t* data, uint16_t count) {
  */
 int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
                                uint16_t count) {
-    /* TODO:IMPLEMENT */
-    return NOT_IMPLEMENTED_ERROR;
+#if 1
+    nrfx_twi_xfer_desc_t xfer_desc;
+    xfer_desc.address = address;
+    xfer_desc.type = NRFX_TWI_XFER_TX;
+    xfer_desc.primary_length = count;
+    xfer_desc.p_primary_buf = data;
+
+    ret_code_t err_code = nrfx_twi_xfer(&m_twi, &xfer_desc, 0);
+    if (NRF_SUCCESS != err_code) {
+      return err_code;
+    }
+    while (nrfx_twi_is_busy(&m_twi)) {
+    }
+#else
+    nrfx_err_t err_code = nrfx_twi_tx(&m_twi, address, data, count, false);
+    if (NRF_SUCCESS != err_code) {
+        return err_code;
+    }
+#endif
+
+    return NO_ERROR;
 }
 
 /**
@@ -114,5 +197,5 @@ int8_t sensirion_i2c_hal_write(uint8_t address, const uint8_t* data,
  * @param useconds the sleep time in microseconds
  */
 void sensirion_i2c_hal_sleep_usec(uint32_t useconds) {
-    /* TODO:IMPLEMENT */
+    nrf_delay_us(useconds);
 }
